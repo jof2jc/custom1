16ba961953c0a8f3348f5506e676394f76163380
jof2jc <jof2jc@gmail.com>
2017-03-28 00:32:51 +0700

    custom1 desk

---
+++ custom1/public/js/conf.js
@@ -0,0 +1,36 @@
+// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+// License: GNU General Public License v3. See license.txt
+
+frappe.provide('erpnext');
+
+// add toolbar icon
+$(document).bind('toolbar_setup', function() {
+	frappe.app.name = "ERPNext";
+
+
+	$('.navbar-home').html('Home');
+
+});
+
+
+
+// doctypes created via tree
+$.extend(frappe.create_routes, {
+	"Customer Group": "Tree/Customer Group",
+	"Territory": "Tree/Territory",
+	"Item Group": "Tree/Item Group",
+	"Sales Person": "Tree/Sales Person",
+	"Account": "Tree/Account",
+	"Cost Center": "Tree/Cost Center"
+});
+
+// preferred modules for breadcrumbs
+$.extend(frappe.breadcrumbs.preferred, {
+	"Item Group": "Stock",
+	"Customer Group": "Selling",
+	"Supplier Type": "Buying",
+	"Territory": "Selling",
+	"Sales Person": "Selling",
+	"Sales Partner": "Selling",
+	"Brand": "Selling"
+});

b4228583f1c7224e4ff25016d3a47f4e8919a0ea
jof2jc <jof2jc@gmail.com>
2019-08-06 01:07:32 +0000

    add field invoice no manually in Sales Invoice

--- custom1/custom1/custom1.py
+++ custom1/custom1/custom1.py
@@ -75,9 +75,10 @@ def set_si_autoname(doc, method):
 		"Request for Quotation"):
 		return
 
-	if "no_invoice" in frappe.db.get_table_columns(doc.doctype) and not doc.amended_from and doc.company in ("PT BLUE OCEAN LOGISTICS"):
+	if "no_invoice" in frappe.db.get_table_columns(doc.doctype) and not doc.amended_from: # and doc.company in ("PT BLUE OCEAN LOGISTICS"):
 		#if doc.no_invoice and not doc.amended_from:
-		doc.name = doc.no_invoice.upper()
+		if doc.no_invoice:
+			doc.name = doc.no_invoice.upper()
 	
 	elif "set_autoname_based_on_posting_date" in frappe.db.get_table_columns("Company") and not doc.name:
 		if frappe.db.get_value("Company",doc.company,"set_autoname_based_on_posting_date"):

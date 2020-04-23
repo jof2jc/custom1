30c1a621efa2d7844a6aab6ab2fbc76132c2c55d
jof2jc <jof2jc@gmail.com>
2020-04-23 05:56:43 +0000

    remove page hub, welcome-to-erpnext from custom1

--- custom1/custom1/page/welcome_to_erpnext/welcome_to_erpnext.js
+++
@@ -1,7 +0,0 @@
-frappe.pages['welcome-to-erpnext'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Welcome to ERPNext',
-		single_column: true
-	});
-}
